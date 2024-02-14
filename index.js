be81fd901b55f3e0c1ceaba4a86a9129b8be64b0
saulius <sjsaulius@gmail.com>
2024-02-14 15:16:03 +0200

    add img code and some window width/height

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+window.addEventListener("load", function () {
+  const canvas = document.querySelector("#canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = this.window.innerWidth;
+  canvas.height = this.window.innerHeight;
+});
