5d61d7841ea85213322320a38b13d7f971ceea8c
saulius <sjsaulius@gmail.com>
2024-02-20 13:46:39 +0200

    extract coordinates and colors from raw pixel data

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ window.addEventListener("load", function () {
   canvas.height = this.window.innerHeight;
 
   class Particle {
-    constructor(effect) {
+    constructor(effect, x, y, color) {
       this.effect = effect;
       this.x = Math.random() * this.effect.width;
       this.y = Math.random() * this.effect.height;
+      this.originX = Math.floor(x);
+      this.originY = Math.floor(y);
+      this.color = color;
       // rectangle size
       this.size = Math.random() * 50;
       // v = velocity
@@ -36,10 +39,25 @@ window.addEventListener("load", function () {
       // how to make to center? half img width and height
       this.x = this.centerX - this.image.width * 0.5;
       this.y = this.centerY - this.image.height * 0.5;
+      this.gap = 5;
     }
     init(context) {
       context.drawImage(this.image, this.x, this.y);
-      const pixels = context.getImageData(0, 0, this.width, this.height);
+      const pixels = context.getImageData(0, 0, this.width, this.height).data;
+      for (let y = 0; y < this.height; y += this.gap) {
+        for (let x = 0; x < this.height; x += this.gap) {
+          const index = (y * this.width + x) * 4;
+          const red = pixels[index];
+          const green = pixels[index + 1];
+          const blue = pixels[index + 2];
+          const alpha = pixels[index + 3];
+          const color = "rgb(" + red + "," + green + "," + blue + ")";
+
+          if (alpha > 0) {
+            this.particlesArray.push(new Particle(this, x, y, color));
+          }
+        }
+      }
     }
     draw(context) {
       this.particlesArray.forEach((particle) => particle.draw(context));
